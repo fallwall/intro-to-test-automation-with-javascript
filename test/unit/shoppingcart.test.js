91c8127f947cdd1f2862cd01eef57ef08aa070ea
XX <fallwall19@gmail.com>
2020-02-25 20:21:32 -0500

    test +

--- test/unit/shoppingcart.test.js
+++ test/unit/shoppingcart.test.js
@@ -1,6 +1,7 @@
 var assert = require('chai').assert;
 
 import { ShoppingCart, Product, LineItem, Shipping } from '../../src/domain';
+import e from 'express';
 
 /**
  * {@link FixedShippingQuoteService} is a utility that we can use with the {@link ShoppingCart}
@@ -30,25 +31,45 @@ describe('ShoppingCart', () => {
 	const socks = new Product("SOCKS", "Crew socks", 500);
 	const discountSocks = new Product("CHEAPSOCKS", "Cheap crew socks", 500, 300);
 
-	describe('...', () => {
+	describe('empty cart', () => {
 
 		const cart = new ShoppingCart(freeShippingQuoteService);
-		cart.updateShipping(shippingInfo);
+    cart.updateShipping(shippingInfo);
+    const purchase = cart.toPurchase();
 
-		it('should ...', () => {
-
+		it('should be zero dollar', () => {
+      assert.equal(purchase.totalPrice, 0);
 		});
 	});
 
-	describe('...', () => {
+	describe('volume discount test', () => {
 
-		const cart = new ShoppingCart(freeShippingQuoteService);
+    const cart = new ShoppingCart(freeShippingQuoteService);
 		cart.addProduct(shirt);
-		cart.updateShipping(shippingInfo);
+    cart.updateShipping(shippingInfo);
+    const purchase = cart.toPurchase();
 
-		it('should ...', () => {
-
+    it('$2500 shirt should get 20% off.', () => {
+      assert.equal(purchase.totalDiscount, 500);
 		});
-	});
+  });
+});
 
+describe('multi-buy test', () => {
+	const shippingInfo = new Shipping.ShippingInfo(
+		Shipping.ShippingService.USPSPriorityMail,
+		"30 W 21st Street, New York, NY 10010"
+	);
+	const freeShippingQuoteService = new FixedShippingQuoteService(0);
+  const cart = new ShoppingCart(freeShippingQuoteService);
+  const shirt = new Product("SHIRT", "Shirt", 2500);
+  cart.addProduct(shirt);
+  cart.addProduct(shirt);
+  cart.addProduct(shirt);
+  cart.updateShipping(shippingInfo);
+  const purchase = cart.toPurchase();
+
+  it('3 purchase should get 10% off.', () => {
+    assert.equal(purchase.totalPurcase, 2250);
+  });
 });
